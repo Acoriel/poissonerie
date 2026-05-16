8deeb138c6c4ae0301fe445007357c3faff0bdcc
Acoriel <81636391+Acoriel@users.noreply.github.com>
2026-05-16 23:38:15 +0100

    Update firebase-sync.js

--- firebase-sync.js
+++ firebase-sync.js
@@ -15,7 +15,7 @@
 // ── CONFIGURATION FIREBASE (à remplacer) ──────────────────────────────────────
 <script type="module">
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
